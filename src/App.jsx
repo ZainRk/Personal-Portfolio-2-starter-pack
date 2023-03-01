8bf2cfa8a173c7669db11f77b145148d66cea293
CaaaseyD <baimaxsolution@gmail.com>
2023-03-01 17:06:11 +1100

    navbar added, css not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Header from './components/Header/Header'
+import css from './styles/app.module.scss'
+
 const App = () => {
-  //don't forget to add font link in index.html
-  return <h1>Subscribe Zainkeepscode</h1>;
+  return <div className={`bg-primary ${css.container}`}>
+    <Header/>
+  </div>;
 };
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import css from './Header.module.scss';
+import {motion} from 'framer-motion';
+import { getMenuStyles, headerVariants } from '../../utils/motion';
+import { BiMenuAltRight} from 'react-icons/bi';
+import useHeaderShadow from '../../hooks/useHeaderShadow';
+
+const Header = () => {
+    const [menuOpened, setMenuOpened] = useState(false);
+    const headerShadow = useHeaderShadow();
+    return(
+        <motion.div
+         initial="hidden"
+         whileInView="show"
+         variants={headerVariants}
+         viewport={{once: false, amount: 0.25}}
+         className={`paddings ${css.wrapper}`}
+         style={{boxShadow: headerShadow}}>
+            <div className={`flexCenter innerWidth ${css.container}`}>
+                <div className={css.name}>CaseyD</div>
+            </div>
+            <ul
+            style={getMenuStyles(menuOpened)}
+            className={`flexCenter ${css.menu}`}>
+                <li><a href="">About</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Experiments</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div className={css.menuIcon}
+            onClick={()=>setMenuOpened((prev)=>!prev)}>
+                <BiMenuAltRight size={30}/>
+            </div>
+        </motion.div>
+    )
+}
+export default Header
