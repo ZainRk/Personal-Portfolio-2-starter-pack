9a4d17880a1588607bf5890331081d151b33cc09
LeonardoSya <2246866774@qq.com>
2023-11-21 21:15:38 +0800

    modified:   index.html 	modified:   src/App.jsx 	new file:   src/components/Header/Header.jsx 	new file:   src/components/Header/Header.module.scss 	modified:   src/main.jsx 	new file:   src/styles/app.module.scss 	modified:   src/styles/index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './styles/index.css'
+import './styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
